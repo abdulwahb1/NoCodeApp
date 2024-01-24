cc76e8f5768948ce85e0924889d6a247120b638c
Abdul Wahab <abdulwabby@gmail.com>
2024-01-25 01:32:44 +0500

    fixed team layout

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -1,10 +1,10 @@
+import React from "react";
 import Image from "next/image";
-import Link from "next/link";
 
 const Team = () => {
   return (
-    <section className="flex flex-col bg-dpurple w-[1400px] h-[992px] items-center">
-      <div className="flex flex-col mt-[100px] w-[880px] h-[96px]">
+    <section className="flex flex-col justify-center items-center bg-dpurple w-[1400px] h-[992px]">
+      <div className="flex flex-col w-[764px] text-center">
         <h1 className="text-4xl text-center text-white font-bold font-dm">
           Startup Crew
         </h1>
@@ -14,215 +14,195 @@ const Team = () => {
           new blocks from UI Kit.
         </p>
       </div>
-      <div className="flex flex-row mt-[150px] flex-wrap w-[848px]">
+      <div className="flex flex-row w-[700px] h-[543px] flex-wrap mt-[70px]">
         <div className="flex flex-col">
           <Image
             src="/p1.svg"
             width={100}
             height={100}
-            alt="Picture of crew member"
+            alt="Picture of the team"
           />
-          <p className="font-dm text-[22px] font-light leading-8 text-white mt-[17px] ml-[5px]">
-            Leah Saloman
-          </p>
-          <p className="font-dm text-[16px] font-light leading-8 text-white mt-[8.2px] ml-[5px]">
-            UI Designer
-          </p>
-          <div className="flex flex-row ml-1 mt-[20px]">
-            <Link href="/">
+          <div className="flex flex-col">
+            <p className="font-dm text-white font-medium text-[22px] mt-[17px]">
+              Leah Salomon
+            </p>
+            <p className="font-dm text-white font-normal text-[16px] mt-[8.2px]">
+              UI Designer
+            </p>
+            <div className="flex flex-row mt-[20px]">
               <Image
                 src="/twitter.svg"
-                width={20}
-                height={20}
-                alt="Twitter logo"
+                width={17}
+                height={38}
+                alt=" twitter icon"
               />
-            </Link>
-            <Link href="/">
               <Image
                 src="/facebook.png"
-                width={20}
-                height={20}
-                alt="Facebook logo"
+                width={17}
+                height={38}
+                alt=" facebook icon"
                 className="ml-[20px]"
               />
-            </Link>
-            <Link href="/">
               <Image
                 src="/instagram.png"
-                width={20}
-                height={20}
-                alt="instagram logo"
+                width={17}
+                height={38}
+                alt="ig icon"
                 className="ml-[20px]"
               />
-            </Link>
+            </div>
           </div>
         </div>
-        <div className="flex flex-col ml-[200px]">
+        <div className="flex flex-col ml-[100px]">
           <Image
             src="/p2.svg"
             width={100}
             height={100}
-            alt="Picture of crew member"
+            alt="Picture of the team"
           />
-          <p className="font-dm text-[22px] font-light leading-8 text-white mt-[17px] ml-[5px]">
-            Colin Timmons
-          </p>
-          <p className="font-dm text-[16px] font-light leading-8 text-white mt-[8.2px] ml-[5px]">
-            UX Designer
-          </p>
-          <div className="flex flex-row ml-1 mt-[20px]">
-            <Link href="/">
+          <div className="flex flex-col">
+            <p className="font-dm text-white font-medium text-[22px] mt-[17px]">
+              Colin Timmons
+            </p>
+            <p className="font-dm text-white font-normal text-[16px] mt-[8.2px]">
+              UX Designer
+            </p>
+            <div className="flex flex-row mt-[20px]">
               <Image
                 src="/twitter.svg"
-                width={20}
-                height={20}
-                alt="Twitter logo"
+                width={17}
+                height={38}
+                alt=" twitter icon"
               />
-            </Link>
-            <Link href="/">
               <Image
                 src="/facebook.png"
-                width={20}
-                height={20}
-                alt="Facebook logo"
+                width={17}
+                height={38}
+                alt=" facebook icon"
                 className="ml-[20px]"
               />
-            </Link>
-            <Link href="/">
               <Image
                 src="/instagram.png"
-                width={20}
-                height={20}
-                alt="instagram logo"
+                width={17}
+                height={38}
+                alt="ig icon"
                 className="ml-[20px]"
               />
-            </Link>
+            </div>
           </div>
         </div>
-        <div className="flex flex-col ml-[200px]">
+        <div className="flex flex-col ml-[100px]">
           <Image
             src="/p3.svg"
             width={100}
             height={100}
-            alt="Picture of crew member"
+            alt="Picture of the team"
           />
-          <p className="font-dm text-[22px] font-light leading-8 text-white mt-[17px] ml-[5px]">
-            Miguel Osborne
-          </p>
-          <p className="font-dm text-[16px] font-light leading-8 text-white mt-[8.2px] ml-[5px]">
-            Frontend Developer
-          </p>
-          <div className="flex flex-row ml-1 mt-[20px]">
-            <Link href="/">
+          <div className="flex flex-col">
+            <p className="font-dm text-white font-medium text-[22px] mt-[17px]">
+              Miguel Osborne
+            </p>
+            <p className="font-dm text-white font-normal text-[16px] mt-[8.2px]">
+              Frontend Developer
+            </p>
+            <div className="flex flex-row mt-[20px]">
               <Image
                 src="/twitter.svg"
-                width={20}
-                height={20}
-                alt="Twitter logo"
+                width={17}
+                height={38}
+                alt=" twitter icon"
               />
-            </Link>
-            <Link href="/">
               <Image
                 src="/facebook.png"
-                width={20}
-                height={20}
-                alt="Facebook logo"
+                width={17}
+                height={38}
+                alt=" facebook icon"
                 className="ml-[20px]"
               />
-            </Link>
-            <Link href="/">
               <Image
                 src="/instagram.png"
-                width={20}
-                height={20}
-                alt="instagram logo"
+                width={17}
+                height={38}
+                alt="ig icon"
                 className="ml-[20px]"
               />
-            </Link>
+            </div>
           </div>
         </div>
-        <div className="flex flex-col mt-[60.8px]">
+        <div className="flex flex-col">
           <Image
             src="/p4.svg"
             width={100}
             height={100}
-            alt="Picture of crew member"
+            alt="Picture of the team"
           />
-          <p className="font-dm text-[22px] font-light leading-8 text-white mt-[17px] ml-[5px]">
-            Taylor Simon
-          </p>
-          <p className="font-dm text-[16px] font-light leading-8 text-white mt-[8.2px] ml-[5px]">
-            Product Manager
-          </p>
-          <div className="flex flex-row ml-1 mt-[20px]">
-            <Link href="/">
+          <div className="flex flex-col">
+            <p className="font-dm text-white font-medium text-[22px] mt-[17px]">
+              Taylor Simon
+            </p>
+            <p className="font-dm text-white font-normal text-[16px] mt-[8.2px]">
+              Product Manager
+            </p>
+            <div className="flex flex-row mt-[20px]">
               <Image
                 src="/twitter.svg"
-                width={20}
-                height={20}
-                alt="Twitter logo"
+                width={17}
+                height={38}
+                alt=" twitter icon"
               />
-            </Link>
-            <Link href="/">
               <Image
                 src="/facebook.png"
-                width={20}
-                height={20}
-                alt="Facebook logo"
+                width={17}
+                height={38}
+                alt=" facebook icon"
                 className="ml-[20px]"
               />
-            </Link>
-            <Link href="/">
               <Image
                 src="/instagram.png"
-                width={20}
-                height={20}
-                alt="instagram logo"
+                width={17}
+                height={38}
+                alt="ig icon"
                 className="ml-[20px]"
               />
-            </Link>
+            </div>
           </div>
         </div>
-        <div className="flex flex-col ml-[200px] mt-[60.8px] ">
+        <div className="flex flex-col ml-[100px]">
           <Image
             src="/p5.svg"
             width={100}
             height={100}
-            alt="Picture of crew member"
+            alt="Picture of the team"
           />
-          <p className="font-dm text-[22px] font-light leading-8 text-white mt-[17px] ml-[5px]">
-            Steven MacAlister
-          </p>
-          <p className="font-dm text-[16px] font-light leading-8 text-white mt-[8.2px] ml-[5px]">
-            Copywriter
-          </p>
-          <div className="flex flex-row ml-1 mt-[20px]">
-            <Link href="/">
+          <div className="flex flex-col">
+            <p className="font-dm text-white font-medium text-[22px] mt-[17px]">
+              Steven MacAlister
+            </p>
+            <p className="font-dm text-white font-normal text-[16px] mt-[8.2px]">
+              Copywriter
+            </p>
+            <div className="flex flex-row mt-[20px]">
               <Image
                 src="/twitter.svg"
-                width={20}
-                height={20}
-                alt="Twitter logo"
+                width={17}
+                height={38}
+                alt=" twitter icon"
               />
-            </Link>
-            <Link href="/">
               <Image
                 src="/facebook.png"
-                width={20}
-                height={20}
-                alt="Facebook logo"
+                width={17}
+                height={38}
+                alt=" facebook icon"
                 className="ml-[20px]"
               />
-            </Link>
-            <Link href="/">
               <Image
                 src="/instagram.png"
-                width={20}
-                height={20}
-                alt="instagram logo"
+                width={17}
+                height={38}
+                alt="ig icon"
                 className="ml-[20px]"
               />
-            </Link>
+            </div>
           </div>
         </div>
       </div>
